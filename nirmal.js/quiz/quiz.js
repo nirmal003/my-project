947117c6a0481408dba665ac133464e2028d5145
maheshwaran-p <mpmahesh1022@gmail.com>
2022-12-05 08:49:00 +0530

    crud

--- nirmal.js/quiz/quiz.js
+++ nirmal.js/quiz/quiz.js
@@ -1,50 +1,54 @@
 const quizData = [
-    {
-        question: 'What is your favorite food?',
-        a: 'Briyani',
-        b: 'Rice with sumbar',
-        c: 'Pongal',
-        d: 'Fried rice',
-        correct: 'c'
-    }, {
-        question: 'What is your choice in sunday?',
-        a: 'Watching movie',
-        b: 'sleeping',
-        c: 'Shopping',
-        d: 'Playing games',
-        correct: 'b'
-    }, {
-        question: 'What is your favorite area?',
-        a: 'Chennai',
-        b: 'Hyderabad',
-        c: 'Mumbai',
-        d: 'Bengalore',
-        correct: 'd'
-    }, {
-        question: 'What will you choose to go for Travel?',
-        a: 'Car',
-        b: 'Train',
-        c: 'Bike',
-        d: 'Bus',
-        correct: 'a'
-    }, {
-        question: 'What will you think about work?',
-        a: 'for money',
-        b: 'for hobbis',
-        c: 'for timepass',
-        d: 'for future',
-        correct: 'd'
-    },
+  {
+    question: "What is your favorite food?",
+    a: "Briyani",
+    b: "Rice with sumbar",
+    c: "Pongal",
+    d: "Fried rice",
+    correct: "c",
+  },
+  {
+    question: "What is your choice in sunday?",
+    a: "Watching movie",
+    b: "sleeping",
+    c: "Shopping",
+    d: "Playing games",
+    correct: "b",
+  },
+  {
+    question: "What is your favorite area?",
+    a: "Chennai",
+    b: "Hyderabad",
+    c: "Mumbai",
+    d: "Bengalore",
+    correct: "d",
+  },
+  {
+    question: "What will you choose to go for Travel?",
+    a: "Car",
+    b: "Train",
+    c: "Bike",
+    d: "Bus",
+    correct: "a",
+  },
+  {
+    question: "What will you think about work?",
+    a: "for money",
+    b: "for hobbis",
+    c: "for timepass",
+    d: "for future",
+    correct: "d",
+  },
 ];
 
 const quizEl = document.getElementById("quiz");
 const answerEl = document.querySelectorAll(".answer");
-const qusEl = document.getElementById('question');
-const a_El = document.getElementById('a_text');
-const b_El = document.getElementById('b_text');
-const c_El = document.getElementById('c_text');
-const d_El = document.getElementById('d_text');
-const submitbtn = document.getElementById('submit');
+const qusEl = document.getElementById("question");
+const a_El = document.getElementById("a_text");
+const b_El = document.getElementById("b_text");
+const c_El = document.getElementById("c_text");
+const d_El = document.getElementById("d_text");
+const submitbtn = document.getElementById("submit");
 
 let current_qus = 0;
 let score = 0;
@@ -52,54 +56,49 @@ let score = 0;
 loadQuiz();
 
 function loadQuiz() {
-    deselectAnswers();
+  deselectAnswers();
 
-    const current_quiz = quizData[current_qus];
+  const current_quiz = quizData[current_qus];
 
-    qusEl.innerHTML = current_quiz.question;
-    a_El.innerHTML = current_quiz.a;
-    b_El.innerHTML = current_quiz.b;
-    c_El.innerHTML = current_quiz.c;
-    d_El.innerHTML = current_quiz.d;
+  qusEl.innerHTML = current_quiz.question;
+  a_El.innerHTML = current_quiz.a;
+  b_El.innerHTML = current_quiz.b;
+  c_El.innerHTML = current_quiz.c;
+  d_El.innerHTML = current_quiz.d;
 }
 
 function getSelected() {
+  let answer = undefined;
 
-    let answer = undefined;
+  answerEl.forEach((answers) => {
+    if (answers.checked) {
+      answer = answers.id;
+    }
+  });
 
-    answerEl.forEach((answers) => {
-
-        if (answers.checked) {
-            answer = answers.id;
-        }
-    });
-
-    return answer;
+  return answer;
 }
 
 function deselectAnswers() {
-
-    answerEl.forEach((answers) => {
-        answers.checked = flase;
-    });
+  answerEl.forEach((answers) => {
+    answers.checked = flase;
+  });
 }
 
 submitbtn.addEventListener("click", () => {
+  const answer = getSelected();
 
-    const answer = getSelected();
-
-    if (answer) {
-        if (answer === quizData[current_qus].correct) {
-            score++;
-        }
-
-        current_qus++;
-
-        if (current_qus < quizData.length) {
-            loadQuiz();
-        }
-        else {
-            quizEl.innerHTML = hi;
-        }
+  if (answer) {
+    if (answer === quizData[current_qus].correct) {
+      score++;
     }
+
+    current_qus++;
+
+    if (current_qus < quizData.length) {
+      loadQuiz();
+    } else {
+      quizEl.innerHTML = "Successfully Completed";
+    }
+  }
 });
