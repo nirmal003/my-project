f7ecd565cdd98820cfae74cacac952a1ea1e243c
maheshwaran-p <mpmahesh1022@gmail.com>
2022-12-14 12:23:01 +0530

    redux

--- nirmal.redux/index.js
+++ nirmal.redux/index.js
@@ -1,19 +1,122 @@
-const BUY_CAKE = "BUY_CAKE";
+const redux = require("redux");
+const createStore = redux.createStore;
+const combineReducers = redux.combineReducers;
+
+const BUY_SWEET = "Buy_Sweet";
+const BUY_CAKE = "Buy_Cake";
+const BUY_EGG = "Buy_Egg";
+const BUY_COFFEE = "Buy_Coffee";
 
 const buySweet = () => {
   return {
-    type: BUY_CAKE,
-    info: "will take sweets",
+    type: BUY_SWEET,
+    info: "will take Sweets",
   };
 };
 
-const initialState = {
+const buyCake = () => {
+  return {
+    type: BUY_CAKE,
+    info: "will take Cakes",
+  };
+};
+
+const buyEgg = () => {
+  return {
+    type: BUY_EGG,
+    info: "will take Eggs",
+  };
+};
+
+const buyCoffee = () => {
+  return {
+    type: BUY_COFFEE,
+    info: "will take Coffes",
+  };
+};
+
+const initialSweet = {
   noOfSweets: 100,
 };
 
-const reducerSweets = (state = initialState, action) => {
+const initialCake = {
+  noOfCakes: 100,
+};
+
+const initialEgg = {
+  noOfEggs: 100,
+};
+
+const initialCoffee = {
+  noOfCoffees: 100,
+};
+
+const reduceSweet = (state = initialSweet, action) => {
   switch (action.type) {
-    case "BUY_CAKE":
-      return { noOfSweets: state.noOfSweets - 1 };
+    case BUY_SWEET:
+      return {
+        ...state,
+        noOfSweets: state.noOfSweets - 5,
+      };
+    default:
+      return state;
   }
 };
+
+const reduceCake = (state = initialCake, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state,
+        noOfCakes: state.noOfCakes - 4,
+      };
+    default:
+      return state;
+  }
+};
+
+const reduceEgg = (state = initialEgg, action) => {
+  switch (action.type) {
+    case BUY_EGG:
+      return {
+        ...state,
+        noOfEggs: state.noOfEggs - 3,
+      };
+    default:
+      return state;
+  }
+};
+
+const reduceCoffee = (state = initialCoffee, action) => {
+  switch (action.type) {
+    case BUY_COFFEE:
+      return {
+        ...state,
+        noOfCoffees: state.noOfCoffees - 2,
+      };
+    default:
+      return state;
+  }
+};
+
+const rootReducer = combineReducers({
+  Sweet: reduceSweet,
+  Cake: reduceCake,
+  Egg: reduceEgg,
+  Coffee: reduceCoffee,
+});
+
+const store = createStore(rootReducer);
+
+console.log("Initial State", store.getState());
+
+const unSubscribe = store.subscribe(() =>
+  console.log("Updated State", store.getState())
+);
+
+store.dispatch(buySweet());
+store.dispatch(buyCake());
+store.dispatch(buyEgg());
+store.dispatch(buyCoffee());
+
+unSubscribe();
